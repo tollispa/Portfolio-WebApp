26f403b302dc1266bbf1384f64c3e35940802ac4
tollispa <112690202+tollispa@users.noreply.github.com>
2023-03-30 15:30:17 +0200

    Changes Logo to react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
     return (
        
@@ -9,10 +10,9 @@ const Footer = () => {
 <p className="font-bold">By Tollis Papadopoulos</p>
 
 <div className="flex mt-4 gap-4">
-<img style={{height: "40px", width: "40px", borderRadius: "10px"}}src="https://th.bing.com/th/id/R.91587cfaaa734bd911942c567e4af1b9?rik=mYwLzGAzuNZD4Q&pid=ImgRaw&r=0" alt="/"/>
-<img style={{height: "40px", width: "40px", objectFit: "cover", borderRadius: "10px"}}src="https://th.bing.com/th/id/R.76b3115cd11a3235062a9b06e6d4424b?rik=2qN5rdsulE3wCg&riu=http%3a%2f%2fclipartmag.com%2fimage%2finstagram-logo-drawing-31.png&ehk=BgPxxTqHFjXMJzc%2bjJmPnFFFyPmnWppD3v9jbjUS%2bvU%3d&risl=&pid=ImgRaw&r=0g" alt="/"/>
-<img style={{height: "40px", width: "40px", objectFit: "cover", borderRadius: "10px"}}src="https://th.bing.com/th/id/OIP.PyjVd_wWa9bz8mem9GcAegAAAA?pid=ImgDet&rs=1" alt="/"/>
-
+<FaLinkedin style={{color: "#0077B5"}}size={40}/>
+<FaInstagram style={{color: "#E1306C"}}size={40}/>
+<FaGithub style={{color: "grey"}}size={40}/>
 </div>
 
 </div>
