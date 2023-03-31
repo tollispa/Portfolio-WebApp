07c7914ee4c0f3bb9ae1d86d52acd2334921d7c2
tollispa <112690202+tollispa@users.noreply.github.com>
2023-03-31 11:37:59 +0200

    Put in Skills Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home"
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 
 
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Navbar/>
        <Home/>
+       <Skills/>
        <Projects/>
        <Footer/>
       
