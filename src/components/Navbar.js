1737f589bee3f8edd8801f5525c7a8fa16cd79b8
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-07 09:41:05 +0200

    Added cursor-pointer in my Navbar Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="text-white flex justify-between items-center h-24 mx-auto px-4 max-w-[1240px]">
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">Tollis Papadopoulos.</h1>
         <ul className="hidden md:flex">
-            <li className="p-4">
+            <li className="p-4 cursor-pointer">
             <Link
                      to="skills"
                      smooth={true}
@@ -25,7 +25,7 @@ const Navbar = () => {
                     Skills
                     </Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 cursor-pointer">
                     <Link
                      to="projects"
                      smooth={true}
@@ -37,7 +37,7 @@ const Navbar = () => {
                      Projects
                     </Link>
 </li>
-         <li className="p-4">
+         <li className="p-4 cursor-pointer">
                <a href="mailto:tollis.123@hotmail.com">Email</a>
          </li>
 
@@ -49,7 +49,7 @@ const Navbar = () => {
         <div className={!nav ? "absolute left-0 top-0 w-[60%] border-r border-r-gray-1200 bg-black ease-in-out duration-500 md:hidden" : "fixed left-[-100%]"}>
        
         <ul className="uppercase p-4">
-            <li className="p-4 border-b border-gray-600">
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
             <Link
                      to="skills"
                      smooth={true}
@@ -61,7 +61,7 @@ const Navbar = () => {
                     Skills
                     </Link>
             </li>
-            <li className="p-4 border-b border-gray-600">
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
             <Link
                      to="projects"
                      smooth={true}
@@ -73,7 +73,7 @@ const Navbar = () => {
                      Projects
                     </Link>
             </li>
-            <li className="p-4 border-b border-gray-600">
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
             <a href="mailto:tollis.123@hotmail.com">Email</a>
             </li>
 
