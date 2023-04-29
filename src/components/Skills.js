d984a82dc0042e598e7125cc0ab576e6344ff5bd
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-29 11:45:14 +0200

    Skills section styling changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,15 +5,15 @@ function Skills() {
     return ( 
         <div id="skills"className="w-full h-[250px] bg-black text-white">
             <h1 className='pt-6 flex justify-center items-center text-4xl font-bold'>Skills and knowlegde</h1>
-            <div className='flex py-3 gap-3 pt-10 justify-center items-center'>
+            <div className='flex py-3 gap-3 sm:gap-10 pt-10 justify-center items-center'>
 
             <FaReact size={70} className="bg-gradient-to-br from-blue-500 to-green-500 rounded hover:scale-110 transform transition duration-300" />
-<FaGitSquare size={70} className="bg-gray-700 hover:scale-110 transform transition duration-300 rounded" />
-<FaHtml5 size={70} className="bg-red-600 rounded hover:scale-110 transform transition duration-300" />
-<FaCss3Alt size={70} className="bg-blue-600 rounded hover:scale-110 transform transition duration-300" />
-<FaDatabase size={70} className="bg-blue-200 rounded hover:scale-110 transform transition duration-300" />
+<FaGitSquare size={70} className="p-1 bg-gray-700 hover:scale-110 transform transition duration-300 rounded" />
+<FaHtml5 size={70} className="p-1 bg-red-600 rounded hover:scale-110 transform transition duration-300" />
+<FaCss3Alt size={70} className="p-1 bg-blue-600 rounded hover:scale-110 transform transition duration-300" />
+<FaDatabase size={70} className="p-1 bg-blue-200 rounded hover:scale-110 transform transition duration-300" />
 
-<FaJsSquare size={70} className="bg-gradient-to-br rounded from-yellow-400 to-orange-500" />
+<FaJsSquare size={70} className="p-1 bg-gradient-to-br rounded from-yellow-400 to-orange-500" />
 
             </div>
             <p className='font-bold text-sm pt-10'>React, Git, HTML, CSS, SQL, Javascript</p>
